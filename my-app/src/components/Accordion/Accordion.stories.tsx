307406316ba3cc56edcb3ed119e370c0d59cc7fd
DmitryShastel <dmshastel@gmail.com>
2021-01-19 19:57:46 +0300

    React c.b.c prosta 11.2 Storybook

--- my-app/src/components/Accordion/Accordion.stories.tsx
+++ my-app/src/components/Accordion/Accordion.stories.tsx
@@ -1,18 +1,80 @@
 import React, {useState} from 'react';
 import {action} from "@storybook/addon-actions";
-import {Accordion} from "./Accordion";
+import {Accordion, AccordionPropsType} from "./Accordion";
+import {Story} from "@storybook/react";
+
+const GetCategoryObj = (categoryName: 'Color' | 'Event' | 'Main') => ({
+    table: {
+        category: categoryName
+    }
+})
 
 export default {
-    title: 'Accordion',
+    title: 'components/Accordion',
     component: Accordion,
+    argTypes: {
+        color: {
+            control: 'color',
+            ...GetCategoryObj('Color')
+        },
+        onChange: {
+           ...GetCategoryObj('Event')
+        },
+        onClick: {
+            ...GetCategoryObj('Event')
+        },
+        items: {...GetCategoryObj('Main')},
+        collapsed: {...GetCategoryObj('Main')},
+        titleValue: {...GetCategoryObj('Main')}
+    }
 }
 
 const callback = action("accordion mode change event fired");
+const onClickCallback = action("some item was clicked")
 
-export const MenuCollapsedMode = () => <Accordion titleValue={"Menu"} collapsed={true} onChange={callback}/>;
-export const UsersUncollapsedMode = () => <Accordion titleValue={"Users"} collapsed={false} onChange={callback}/>;
+const Template: Story<AccordionPropsType> = (args) => <Accordion {...args} />
 
-export const ModeChanging = () => {
+const callbacksProps = {
+    onChange: callback,
+    onClick: onClickCallback
+}
+
+export const MenuCollapsedMode = Template.bind({})
+MenuCollapsedMode.args = {
+    ...callbacksProps,
+    titleValue: "Menu",
+    collapsed: true,
+    items: [],
+}
+
+export const UsersUncollapsedMode = Template.bind({})
+UsersUncollapsedMode.args = {
+    ...callbacksProps,
+    titleValue: "Users",
+    collapsed: false,
+    items: [{title: "Dmitry", value: 1}, {title: "Valera", value: 2},
+        {title: "Artem", value: 3}, {title: "Victor", value: 4}],
+
+}
+
+
+export const ModeChanging: Story<AccordionPropsType>  = (args) => {
     const [value, setvalue] = useState<boolean>(true);
-    return <Accordion titleValue={"Users"} collapsed={value} onChange={()=> setvalue(!value)}/>;
-};
+
+    return <Accordion {...args} collapsed = {value} onChange = {() => setvalue(!value)}/>
+}
+ModeChanging.args = {
+    titleValue: 'Users',
+    items: [
+        {title: 'Dmitry', value: 1},
+        {title: 'Valera', value: 2},
+        {title: 'Artem', value: 3},
+        {title: 'Victor', value: 4}
+    ],
+    onClick: (value) => {
+        alert(`user with ID ${value} should be happy`)
+    }
+}
+
+
+
--- my-app/src/components/Accordion/Accordion.tsx
+++ my-app/src/components/Accordion/Accordion.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 
-type AccordionPropsType = {
+export type AccordionPropsType = {
     titleValue: string
     collapsed: boolean
     onChange: () => void
+    /**
+     * Elements are showed when accordion is opened (not collapsed)
+     */
+    items: {}
+    /**
+     * Elements are showed when is opened. Each item should be ItemType
+     */
+    onClick: (value: any) => void
+    /**
+     * optional color of header text
+     */
+    color?: string
 }
 
 export function Accordion(props: AccordionPropsType) {
     console.log("Accordion rendering")
     return (
         <div>
-            <AccordionTitle title={props.titleValue} onChange={props.onChange}/>
+            <AccordionTitle title={props.titleValue}
+                            color={props.color}
+                            onChange={props.onChange}/>
             {!props.collapsed && <AccordionBody/>}
         </div>
     )
@@ -19,13 +33,15 @@ export function Accordion(props: AccordionPropsType) {
 type AccordionTitlePropsType = {
     title: string
     onChange: () => void
+    color?: string
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log("AccordionTitle rendering")
     return (
         <div>
-            <h3 onClick={(e) =>props.onChange() }>-- {props.title} --</h3>
+            <h3 style = {{color: props.color ? props.color : "black"}}
+                onClick = {(e) => props.onChange()}>-- {props.title} --</h3>
         </div>
     )
 }
