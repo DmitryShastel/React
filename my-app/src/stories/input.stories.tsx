8970d5dd56342325e039facb0ae8062dd86a5694
DmitryShastel <dmshastel@gmail.com>
2021-01-23 09:46:35 +0300

    React k.b.k prosta controlled_input, FLUX

--- my-app/src/stories/input.stories.tsx
+++ my-app/src/stories/input.stories.tsx
@@ -17,7 +17,6 @@ export const TrackingValueOfControlledInput = () => {
 
   return <><input onChange = {onChange}/> - {value}</>
 }
-
 export const GetValueOfUncontrolledByButtonPress = () => {
   const [value, setValue] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
@@ -30,4 +29,33 @@ export const GetValueOfUncontrolledByButtonPress = () => {
   return <><input ref = {inputRef}/>
     <button onClick = {save}>save</button>
        -actual value: {value}</>
+}
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState("");
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.value)
+  }
+  debugger
+
+  return <input value={parentValue} onChange={ onChange }/>
+}
+export const ControlledCheckbox = () => {
+  const [parentValue, setparentValue] = useState(true);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setparentValue(e.currentTarget.checked);
+  }
+  return <input type="checkbox" checked={parentValue} onChange={onChange}/>
+}
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>("none");
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value);
+  }
+  return  <select value={parentValue} onChange={onChange}>
+    <option>none</option>
+    <option value={1}>Minsk</option>
+    <option value={2}>Moscow</option>
+    <option value={3}>Kiev</option>
+  </select>
 }
